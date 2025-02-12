620837a80629e661420f47d39fc4f748a5c326d1
Justin Law <justin.law@defenseunicorns.com>
2025-02-12 17:55:40 -0500

    fix: static website page for gh-pages

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  // NOTE: Prevents hydration mismatch by ensuring client-side only changes
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] overflow-hidden">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 overflow-hidden">
       <div className="text-center text-5xl font-bold mb-8">Justin Law</div>
 
       <main className="row-start-2 flex flex-col items-center justify-center w-full flex-grow">
@@ -10,11 +18,14 @@ export default function Home() {
           className="w-[100%] max-w-[1500px] flex-grow flex"
           style={{ height: "75vh" }}
         >
-          <iframe
-            src="https://drive.google.com/file/d/1o3hw7mOlJ5JB9XfoDQNdv8aBdCVPl8cp/preview"
-            className="w-full h-full border rounded-md shadow-lg"
-            title="Justin Law's Resume"
-          />
+          {/* NOTE: Prevents hydration issue when rendering iframe */}
+          {isClient && (
+            <iframe
+              src="https://drive.google.com/file/d/1o3hw7mOlJ5JB9XfoDQNdv8aBdCVPl8cp/preview"
+              className="w-full h-full border rounded-md shadow-lg"
+              title="Justin Law's Resume"
+            />
+          )}
         </div>
       </main>
 
