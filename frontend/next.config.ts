9837f16e0170f29fffb52288504083e7c5ab8370
Justin Law <justin.law@defenseunicorns.com>
2025-02-12 17:24:23 -0500

    basic resume and links website

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: { unoptimized: true },
   basePath: isProd ? "/justinthelaw.github.io" : "",
   assetPrefix: isProd ? "/justinthelaw.github.io/" : "",
 };
